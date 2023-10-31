4d109290d34e8ed519a4355dba4b8227bdec0b23
Martin Enriquez <martinantraxelm1@gmail.com>
2023-10-31 11:26:50 -0700

    finished working on exercise one

---
+++ objectScripts/scripts/label_maker.js
@@ -0,0 +1,22 @@
+// JavaScript object - creates a file system to hold information
+
+let myInfo = {
+    firstName: "Martin Enriquez ",
+    address: "123 Main N Street",
+    city: "Phoenix",
+    state: "Arizona",
+    zip: "85008"
+
+        
+}
+// calling a function named printContact and passing myInfo object into it.
+printContact(myInfo);
+
+// inside this function I passed the myInfo object
+// then I console logged the properties 
+function printContact(myInfo) {
+    console.log(myInfo.firstName);
+    console.log(myInfo.address);
+    console.log(`${myInfo.city}, ${myInfo.state} ${myInfo.zip}`);
+  
+}
